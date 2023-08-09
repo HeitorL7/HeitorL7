3c08e9e90a03857138c433c131f547d8077ddf9c
HeitorL7 <141830309+HeitorL7@users.noreply.github.com>
2023-08-09 10:02:46 -0300

    Update PROJETO.java

--- PROJETO.java
+++ PROJETO.java
@@ -5,7 +5,15 @@ class CalcularHoras{
         
       //criar um objeto que sera utilizado para criar o id de consulta
       Random consulta = new Random();
-      
-      
+     
+      //garante que tofod od numeros de consulta serao positivos
+     
+          int protocolo = consulta.nextInt(1000)*1234;
+          
+          //pede ao usuario o valor de horas trabalhadas na semana
+          
+          System.out.println("Por favor , anote seu protocolo "+protocolo);
+      }
+    
+        
     }
-}
